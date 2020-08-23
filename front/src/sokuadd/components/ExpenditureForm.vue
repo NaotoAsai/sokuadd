8329bb6fcd92d621c559bccc018b561a02613b99
NaotoAsai <himeringo4854@gmail.com>
2020-08-23 20:31:04 +0900

    first commit(基本動作確認終了)

---
+++ front/src/sokuadd/components/ExpenditureForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row row class="text-xs-center">
+    <v-col md="4" offset-md="4" class="lighten-4">
+      <v-card flat>
+        <v-form>
+          <v-text-field
+            name="username"
+            label="金額"
+            class="ma-12"
+          />
+          <v-overflow-btn
+            :items="expenditure_classes"
+            placeholder="分類"
+            class="ma-12"
+          />
+          <v-card-actions>
+            <v-btn primary large block>
+              追加
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      expenditure_classes: ['aaa', 'bbb', 'ccc']
+    }
+  }
+}
+</script>
